63c91bdfc6415abaecd6549aa41a26b1adf77e21
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-02-09 15:02:02 +0000

    save1

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,7 +2,11 @@ package primeiro_exemplo;
 
 public class Hello {
 public static void main(String[] args) {
+
+    int cont = 10;
+    String nome ="Beatriz";
     System.out.println("Olá Mundo!!");
+    System.out.println("Tudo bem?");
 }
 }
 
